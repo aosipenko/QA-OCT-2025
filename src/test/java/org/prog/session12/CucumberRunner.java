65e6d640b23891e43c9fe003c37088aab2914933
aosipenko <aoskiev@gmail.com>
2025-11-14 19:14:17 +0100

    Session 12: Jenkins upd 7 - Allure Reports

--- src/test/java/org/prog/session12/CucumberRunner.java
+++ src/test/java/org/prog/session12/CucumberRunner.java
@@ -20,7 +20,7 @@ import java.sql.SQLException;
 //TODO: Move previous HW to cucumber BDD, make amount of phones request a step param
 
 @CucumberOptions(
-        features = "src/test/resources/features/session11",
+        features = "src/test/resources/features/session12",
         glue = "org.prog.session12.steps",
         plugin = {
                 "pretty",
